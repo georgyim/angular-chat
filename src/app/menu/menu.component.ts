c6666ebdebd734b45d0f864f564a4c322bb0ea56
djjorik <shoopikbat@gmail.com>
2018-04-15 21:40:22 +0500

    add logout to page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './../services/auth/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  exit() {
+    this.authService.logout();
+    this.router.navigate(['/auth']);
   }
 
 }
